c6692b27776282bfd94b4053ed00bb9ce3427fec
srgrj <someshgarje@gmail.com>
2018-02-27 23:16:42 +0530

    changed pycharm project dir from code to main dir and changed code dir name to chodeplayground

--- Code/forif.py
+++
@@ -1,8 +0,0 @@
-var="hello world"
-varlen=len(var)
-#print(varlen)
-for i in range(0,varlen):
-	if var[i] == " ":
-		print ("\n")
-	else:
-		print (var[i], end='')
---
+++ CodePlayGround/forif.py
@@ -0,0 +1,8 @@
+var = "hello world"
+varlen = len(var)
+# print(varlen)
+for i in range(0, varlen):
+    if var[i] == " ":
+        print("\n")
+    else:
+        print(var[i], end='')
